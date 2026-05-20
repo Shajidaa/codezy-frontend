012a59b112f2ce7e4181f62644b8d2ca396b617b
Shajidaa <shajidaakterlopa101@gmail.com>
2026-05-20 20:47:47 +0600

    add language

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,19 @@
 "use client";
 
-import { Geist, Geist_Mono } from "next/font/google";
+import {  Poppins } from "next/font/google";
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
   import { ToastContainer } from 'react-toastify';
 import WhatsAppWidget from "./(marketing)/components/WhatsAppWidget";
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+
+const poppins = Poppins({
+  variable: "--font-poppins",
   subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800"],
+  
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+
 
 
 export default function RootLayout({
@@ -24,7 +24,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${poppins.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col"><SessionProvider>
           {children}
